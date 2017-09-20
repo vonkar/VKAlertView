25c8f7a816a51e473e14ca03444cb2fb96d77093
Vinod Onkar <vinodonkar@vinods-macbook-pro.local>
2017-09-20 18:11:39 +0530

    Initial Commit

---
+++ VKAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKAlertView
+//
+//  Created by Vinod Onkar on 20/09/17.
+//  Copyright © 2017 Vinod Onkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
